84d13e272b2b95e363ebdea70b6e807cd691ad3e
yeaeun23 <yeaeun23@naver.com>
2025-03-02 22:53:29 +0900

    프로젝트 데이터, 섹션 스타일

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,46 +1,45 @@
 import { CiLink } from "react-icons/ci";
 import { DiGithubBadge } from "react-icons/di";
 
-function Project({ title, description, technologies, link, github }) {
+function Project({ project }) {
   return (
-    <div className="hover:bg-zinc-100 hover:dark:bg-zinc-900 transition-all duration-300 p-6 md:rounded-xl">
-      <div className="flex gap-2 overflow-x-scroll py-2">
-        {/* =========== TECHNOLOGIES USED =========== */}
-        {technologies.split(",").map((tech, index) => (
+    <a
+      target="_blank"
+      href={project.link}
+      title="클릭하세요!"
+      className="hover:bg-zinc-100 hover:dark:bg-zinc-900 transition-all duration-300 p-6 md:rounded-xl"
+    >
+      {
+        /* =========== IMAGE =========== */
+        project.image && (
+          <img
+            src={project.image}
+            alt={project.title}
+            className="w-full h-40 object-cover rounded-xl"
+          />
+        )
+      }
+
+      {/* =========== TITLE =========== */}
+      <h3 className="font-bold text-lg text-zinc-700 dark:text-zinc-300 mt-3">{project.title}</h3>
+
+      {/* =========== DESCRIPTION =========== */}
+      <p className="leading-7 text-zinc-500 dark:text-zinc-300 font-light text-base mt-1">
+        {project.description}
+      </p>
+
+      {/* =========== SKILLS =========== */}
+      <div className="flex gap-2 overflow-x-scroll mt-3">
+        {project.skills.split(",").map((skill, index) => (
           <span
-            className="inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500"
             key={index}
+            className="inline-flex items-center gap-x-1.5 py-1 px-2 rounded-full text-xs font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500 whitespace-nowrap"
           >
-            {tech}
+            {skill}
           </span>
         ))}
       </div>
-      {/* =========== PROJECT TITLE =========== */}
-      <h3 className="font-bold text-lg text-zinc-700 dark:text-zinc-300 mt-4">{title}</h3>
-      {/* =========== PROJECT DESCRIPTION =========== */}
-      <p className="leading-7 text-zinc-500 dark:text-zinc-300 font-light text-base mt-4">
-        {description}
-      </p>
-
-      <div className="flex gap-6 text-zinc-600 dark:text-zinc-300 font-medium">
-        {/* =========== PROJECT LINK =========== */}
-        <a
-          href={link}
-          className="flex gap-2 mt-4 hover:text-red-800 hover:dark:text-red-500 cursor-pointer transition-all duration-300"
-        >
-          <CiLink className="text-2xl self-center" />
-          <span className="text-xs self-center">Preview</span>
-        </a>
-        {/* =========== PROJECT GITHUB =========== */}
-        <a
-          href={github}
-          className="flex gap-2 mt-4 hover:text-red-800 hover:dark:text-red-500 cursor-pointer transition-all duration-300"
-        >
-          <DiGithubBadge className="text-2xl self-center" />
-          <span className="text-xs self-center">Github</span>
-        </a>
-      </div>
-    </div>
+    </a>
   );
 }
 
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -8,16 +8,7 @@ function Projects() {
       className="mt-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-12 lg:px-16"
     >
       {user_info.projects.map((project, index) => {
-        return (
-          <Project
-            key={index}
-            title={project.title}
-            description={project.description}
-            technologies={project.technologies}
-            github={project.github}
-            link={project.link}
-          />
-        );
+        return <Project key={index} project={project} />;
       })}
     </section>
   );
